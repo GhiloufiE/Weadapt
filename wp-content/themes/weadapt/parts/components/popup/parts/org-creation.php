41aeef5b13e95411cf636424467e56a3a5342019
houssembenghozzi <houssembnghozzi@gmail.com>
2024-08-09 11:43:44 +0100

    registration agora updates

--- wp-content/themes/weadapt/parts/components/popup/parts/org-creation.php
+++ wp-content/themes/weadapt/parts/components/popup/parts/org-creation.php
@@ -2,7 +2,7 @@
 	
 	<button class="close" data-popup="org-creation"
 		aria-label="<?php _e('Close', 'weadapt'); ?>"><?php echo get_img('icon-close'); ?></button>
-	<h2 class="popup__header__title" id="forgot-password"><?php _e('Organisation Creation', 'weadapt'); ?></h2>
+	<h2 class="popup__header__title" id="forgot-password"><?php _e('Complete Your Organisation', 'weadapt'); ?></h2>
 </div>
 <div class="popup__content">
 
@@ -13,7 +13,7 @@
 				d="M10.99 15H11M11 21C16.5228 21 21 16.5228 21 11C21 5.47715 16.5228 1 11 1C5.47715 1 1 5.47715 1 11C1 16.5228 5.47715 21 11 21ZM10.995 7L11 12.4993L10.995 7Z"
 				stroke="currentColor" stroke-linecap="square"></path>
 		</svg>
-		<p>When you add your organisation you will be redirected to an interface to fill the necessary informations.</p>
+		<p>Your organisation creation is almost complete. Proceed now to finalize the details, or you can always finish it later by editing your profile.</p>
 	</div>
 	<div class="wp-block-buttons">
 	<div class="wp-block-button">
@@ -33,15 +33,17 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     document.getElementById('maybe-later').addEventListener('click', function() {
-        const popup = document.querySelector('[data-popup="org-creation"]');
-        if (popup) {
-            popup.closest('.popup__content').style.display = 'none';
+        const closeButton = document.querySelector('.close[data-popup="org-creation"]');
+        if (closeButton) {
+            closeButton.click(); 
         }
     });
 });
 </script>
 
-
+<style>.hidden {
+    display: none !important;
+}</style>
 
 <?php echo get_part('components/sign-up/index', ['template' => 'org-creation']); ?>
 </div>
