2b33b4b2a50716e8fb9db55259e6f5d76acf9375
Ghiloufiw <wassim.ghiloufi@esprit.tn>
2024-08-16 23:25:24 +0100

    wait

--- a/script.js
+++ a/script.js
@@ -1,9 +1,9 @@
 var zoomTargets = {
-    red: { translateXPercent: 50, translateYPercent: 20, scale: 2.5 }, 
-    orange: { translateXPercent: 80, translateYPercent: 50, scale: 2.5 },
-    green: { translateXPercent: 50, translateYPercent: 80, scale: 2.5 },
-    blue: { translateXPercent: 20, translateYPercent: 50, scale: 2.5 },
-    rectangle: { translateXPercent: 50, translateYPercent: 50, scale: 2.5 } 
+    red: { translateXPercent: 50, translateYPercent: 80, scale: 2 }, // translateXPercent: 50, translateYPercent: 80, scale: 2 
+    orange: { translateXPercent: 20, translateYPercent: 50, scale: 2 }, // translateXPercent: 20, translateYPercent: 50, scale: 2
+    green: { translateXPercent: 50, translateYPercent: 20, scale: 2 }, // translateXPercent: 50, translateYPercent: 20, scale: 2
+    blue: { translateXPercent: 80, translateYPercent: 50, scale: 2 }, //  translateXPercent: 80, translateYPercent: 50, scale: 2
+    rectangle: { translateXPercent: 50, translateYPercent: 50, scale: 2 } 
 };
 
 d3.selectAll('button[data-target]').on('click', function() {
@@ -19,12 +19,12 @@ d3.selectAll('button[data-target]').on('click', function() {
     var translateX = (container.width * (target.translateXPercent / 100) - container.width / 2) * target.scale;
     var translateY = (container.height * (target.translateYPercent / 100) - container.height / 2) * target.scale;
 
-    d3.select('.diagram').transition().duration(500)
+    d3.select('.diagram').transition().duration(1)
         .style('transform', 'translate(' + translateX + 'px,' + translateY + 'px) scale(' + target.scale + ')');
 });
 
 d3.select('.zoom-out').on('click', function() {
-    d3.select('.diagram').transition().duration(500)
+    d3.select('.diagram').transition().duration(1)
         .style('transform', '');
 });
 
@@ -42,7 +42,7 @@ function refreshButtonHandlers() {
         var translateX = (container.width * (target.translateXPercent / 100) - container.width / 2) * target.scale;
         var translateY = (container.height * (target.translateYPercent / 100) - container.height / 2) * target.scale;
 
-        d3.select('.diagram').transition().duration(500)
+        d3.select('.diagram').transition().duration(1)
             .style('transform', 'translate(' + translateX + 'px,' + translateY + 'px) scale(' + target.scale + ')');
     });
 }
