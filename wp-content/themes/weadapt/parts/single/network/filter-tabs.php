df859492d029f7a85cb95783a4ddad27dd49ef63
Ghiloufiw <wassim.ghiloufi@esprit.tn>
2024-08-29 16:33:44 +0100

    make the about tab first if network id is 28392

--- wp-content/themes/weadapt/parts/single/network/filter-tabs.php
+++ wp-content/themes/weadapt/parts/single/network/filter-tabs.php
@@ -6,48 +6,54 @@
  */
 
 $items = [
-	[
-		'id' => 'tab-latest',
-		'controls' => 'tab-latest-panel',
-		'selected' => true,
-		'label' => __( 'Latest', 'weadapt' ),
-	],
-	[
-		'id' => 'tab-about',
-		'controls' => 'tab-about-panel',
-		'selected' => false,
-		'label' => __( 'About', 'weadapt' ),
-	],
-	[
-		'id' => 'tab-editors',
-		'controls' => 'tab-editors-panel',
-		'selected' => false,
-		'label' => __( 'Editors', 'weadapt' ),
-	],
-	[
-		'id' => 'tab-members',
-		'controls' => 'tab-members-panel',
-		'selected' => false,
-		'label' => __( 'Members', 'weadapt' ),
-	],
-	[
-		'id' => 'tab-organisations',
-		'controls' => 'tab-organisations-panel',
-		'selected' => false,
-		'label' => __( 'Organisations', 'weadapt' ),
-	],
-	[
-		'id' => 'network-forum',
-		'controls' => 'tab-forum-panel',
-		'selected' => false,
-		'label' => __( 'Forum topics', 'weadapt' ),
-	],
+    [
+        'id' => 'tab-latest',
+        'controls' => 'tab-latest-panel',
+        'selected' => true,
+        'label' => __('Latest', 'weadapt'),
+    ],
+    [
+        'id' => 'tab-about',
+        'controls' => 'tab-about-panel',
+        'selected' => false,
+        'label' => __('About', 'weadapt'),
+    ],
+    [
+        'id' => 'tab-editors',
+        'controls' => 'tab-editors-panel',
+        'selected' => false,
+        'label' => __('Editors', 'weadapt'),
+    ],
+    [
+        'id' => 'tab-members',
+        'controls' => 'tab-members-panel',
+        'selected' => false,
+        'label' => __('Members', 'weadapt'),
+    ],
+    [
+        'id' => 'tab-organisations',
+        'controls' => 'tab-organisations-panel',
+        'selected' => false,
+        'label' => __('Organisations', 'weadapt'),
+    ],
+    [
+        'id' => 'network-forum',
+        'controls' => 'tab-forum-panel',
+        'selected' => false,
+        'label' => __('Forum topics', 'weadapt'),
+    ],
 ];
 
-// Check if the current post ID is 28392 and swap the first and second items
-if ( get_the_ID() == 28392 && count($items) > 1 ) {
-    $items[1]['selected'] = true;
-    $items[0]['selected'] = false;
+if (get_the_ID() == 28392 && count($items) > 1) {
+     foreach ($items as $key => $item) {
+        if ($item['id'] === 'tab-about') {
+             $items[$key]['selected'] = true;
+             $aboutTab = array_splice($items, $key, 1);
+            array_unshift($items, $aboutTab[0]);
+            break;
+        }
+    }
+     $items[1]['selected'] = false;
 }
 
-get_part( 'components/single-tabs-nav/index', ['items' => $items] );
+get_part('components/single-tabs-nav/index', ['items' => $items]);
